36bfc362b542b994c1fb23c7cd9416d608728f9b
aesop <aesop@DESKTOP-8UV1A89>
2023-09-30 21:25:29 -0500

    Add project files.

---
+++ WindowsFormsApp1/FpsCounter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class FpsCounter
+    {
+        private long lastTime = DateTime.Now.Ticks;
+        private long thisTime = DateTime.Now.Ticks;
+        private long deltaTime = 0;
+        private int fps = 0;
+        private int averageCounter = 0;
+        private int fpsTotal = 0;
+
+        public int averageFps = 0;
+
+        public int Update()
+        {
+            thisTime = DateTime.Now.Ticks;
+            deltaTime = thisTime - lastTime;
+            lastTime = thisTime;
+
+            fps = (int)(10000000.0 / deltaTime);
+
+            averageCounter++;
+            fpsTotal += fps;
+            if (averageCounter >= 10)
+            {
+                averageFps = fpsTotal / averageCounter;
+            }
+
+            return averageFps;
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/Game1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Game1 : Form
+    {
+        FpsCounter fps = new FpsCounter();
+        
+        public Game1()
+        {
+            var timer = new Timer();
+            timer.Enabled = false;
+            timer.Interval = 1;
+            timer.Tick += new EventHandler(TimerCallback);
+            timer.Start();
+            
+            InitializeComponent();
+        }
+
+        private void TimerCallback(object sender, EventArgs e)
+        {
+            Invalidate();
+            
+            fps.Update();
+
+            return;
+        }
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            
+            g.DrawString(fps.averageFps.ToString(), new Font("Arial", 16), Brushes.Red, 0, 0);
+           
+        }
+    }
+}
